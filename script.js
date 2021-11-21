29b8e7dd43d1540b5f0a21306e610dcceae993c6
email <abe.solis2015@gmail.com>
2021-11-21 15:28:08 -0800

    adjusting ids to match script

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-$(".btn").on("click", function () {
-  var id = $(this).attr("id").split("-")[0]
-  var plan = $("#" + id + "-txt").val()
-  console.log(id, plan)
-  localStorage.setItem(id, plan)
-
+$(".btn").on("click",function(){
+    var id = $(this).attr("id").split("-")[0]
+    var plan = $("#"+ id +"-txt").val()
+    console.log(id, plan)
+    localStorage.setItem(id, plan)
 })
-
-var CurrentDate = moment().format('MMMM Do YYYY, h:mm:ss a')
-$("#currentDay").text(CurrentDate)
-
-var CurrentHour = moment().hours()
-console.log(CurrentDate, CurrentHour)
-
-for (let i = 8; i < 17; i++) {
-  var chose = localStorage.getItem(i)
-  $("#" + i + "-txt").val(chose)
-  if (i < CurrentHour) {
-    $("#" + i + "-txt").addClass("past")
-
-  }
-
-  else if (i === CurrentHour) {
-    $("#" + i + "-txt").addClass("present")
-
-  }
-
-  else {
-    $("#" + i + "-txt").addClass("future")
-  }
-
+var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a')
+$("#currentDay").text(currentDate)
+var currentHour = moment().hours()
+console.log(currentDate,currentHour)
+for (let i = 8; i < 19; i++) {
+var chose = localStorage.getItem(i)
+$("#"+ i + "-txt").val(chose)
+if(i < currentHour) {
+    $("#"+ i + "-txt").addClass("past") 
+    
+}
+    else if (i === currentHour) {
+        $("#"+ i + "-txt").addClass("present")
+    }
+    else {
+        $("#"+ i + "-txt").addClass("future")
+    }
 }
